dd8176e5d5684d7f24d16e892637afec6658cb42
Eric C <the.ricetech@gmail.com>
2018-10-21 23:29:07 -0400

    Basic Structure
    Added a couple of functions and basic code.
    Still need to add random events and chef requests.

--- culinaryCombinations.py
+++ culinaryCombinations.py
@@ -3,20 +3,50 @@
 # For MDM4UE-B, Ms. S. Sajan
 
 from random import randint
+from collections import Counter
 
 
 class Player:
+    name = ""
     points = 0
-    ingredients = []
     pos = 0
-
-    def __init__(self, name):
-        self.name = name
+    ingredients = Counter(["proteins", "veggies", "veggies", "carbs", "sauces"])
 
 
-def init():
+def input_check(low, high):
+    while False:
+        try:
+            check = int(input())
+
+            if low <= check <= high:
+                return check
+            else:
+                print("Error: Input out of range (" + str(low) + ", " + str(high), ")")
+        except ValueError:
+            print("Error: Not an integer. Try again.")
+
+
+def chef_request():
     c1 = ["proteins", "veggies", "veggies", "carbs", "sauces"]
-    c2 = ["proteins"]
+
+    return c1
+
+
+def actions(pos):  # Combine with pantry() and rand_event()
+    # Define 1st space
+    start = 1
+    # Define spaces where player picks up a ingredient
+    food = (2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16)
+    # Define spaces where a random event occurs
+    randEvent = (17, 18, 19, 20, 21)
+    if pos == start:
+        return "s"
+    if pos in food:
+        return "f"
+    if pos in randEvent:
+        return "r"
+    else:
+        return "n"
 
 
 def pantry():
@@ -43,3 +73,27 @@ def pantry():
     card = randint(0, len(category) - 1)
 
     return category, category[card]
+
+
+def random_event():
+    print("This function has not been implemented yet.")
+
+
+# Main code
+roundCurrent = 1
+roundLimit = 5
+
+print("Number of players:")
+playerNum = input_check(1, 20)
+players = [Player() for i in range(playerNum)]
+
+for p in players:
+    p.name = input("Player " + str(p) + ": Please enter your name: \n")
+    if p.name == "" or " ":
+        p.name = "Player"
+
+for roundCurrent in range(1, roundLimit):
+    for p in players:
+        roll = randint(1, 12)
+        p.pos += roll
+        actions(p.pos)
